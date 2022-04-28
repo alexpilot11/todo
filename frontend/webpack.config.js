b4bffdc242c95786f35ad0ba19a28d09c13daa67
Thompson <pthompson64@elsys.gtri.org>
2022-04-28 15:27:40 -0400

    webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    // Code for this file and .babelrc copied from https://www.valentinog.com/blog/tutorial-api-django-rest-react/
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx'],
+    },
+};
